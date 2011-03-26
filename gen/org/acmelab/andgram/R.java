16134173e018a0f6a24cfcd0e8649908d9b0a0f2
Mark L. Chang <mark.chang@gmail.com>
2011-03-26 01:05:21 -0400

    removed stub image

--- gen/org/acmelab/andgram/R.java
+++ gen/org/acmelab/andgram/R.java
@@ -11,15 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int camera_stub=0x7f020000;
-        public static final int ic_menu_compose=0x7f020001;
-        public static final int ic_menu_delete=0x7f020002;
-        public static final int ic_menu_login=0x7f020003;
-        public static final int ic_menu_preferences=0x7f020004;
-        public static final int ic_menu_refresh=0x7f020005;
-        public static final int ic_menu_slideshow=0x7f020006;
-        public static final int icon=0x7f020007;
-        public static final int stub=0x7f020008;
+        public static final int ic_menu_compose=0x7f020000;
+        public static final int ic_menu_delete=0x7f020001;
+        public static final int ic_menu_login=0x7f020002;
+        public static final int ic_menu_preferences=0x7f020003;
+        public static final int ic_menu_refresh=0x7f020004;
+        public static final int ic_menu_slideshow=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int stub=0x7f020007;
     }
     public static final class id {
         public static final int btnLogin=0x7f060008;
